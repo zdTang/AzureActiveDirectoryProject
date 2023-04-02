c37c0c5ac86a8896d45c8710e334dd772fbce134
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-04-02 11:41:55 -0400

    3-4 Add authentication code to Web api

--- AzureADApi/Program.cs
+++ AzureADApi/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+    {
+        options.Authority = "https://login.microsoftonline.com/5950d41d-9b21-4de2-bd66-bd8e54f0bd86/v2.0";  //same as last project, copied from there
+        options.Audience = "api://f70e9921-f18f-4613-a455-e262780e9d56";  // the Id in the top of "Expose API" VIEW
+    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,7 +26,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
