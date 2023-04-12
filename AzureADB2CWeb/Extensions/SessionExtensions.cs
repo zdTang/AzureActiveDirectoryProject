32014516d927e3b672527f4756cef242cfcd002c
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-04-12 14:36:10 -0400

    5-4 Session Extensions

---
+++ AzureADB2CWeb/Extensions/SessionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace AzureADB2CWeb.Extensions
+{
+    public static class SessionExtensions
+    {
+        public static T? GetComplexData<T>(this ISession session, string key)
+        {
+            var data = session.GetString(key);
+            if (data == null)
+            {
+                return default;
+            }
+            return JsonConvert.DeserializeObject<T>(data);
+        }
+
+
+        // Do we need using the T here ? I prefer deleting it !!!
+        //public static void SetComplexData<T>(this ISession session, string key,object value)
+        public static void SetComplexData(this ISession session, string key, object value)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+    }
+}
