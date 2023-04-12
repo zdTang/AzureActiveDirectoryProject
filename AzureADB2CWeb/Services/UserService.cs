220518abd8ad0717ffb72d63bd52ae9d77016ba7
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-04-12 15:32:17 -0400

    5-6 User Service Implementation

--- AzureADB2CWeb/Services/UserService.cs
+++ AzureADB2CWeb/Services/UserService.cs
@@ -1,34 +1,65 @@
 ﻿using AzureADB2CWeb.Data;
+using AzureADB2CWeb.Extensions;
 using AzureADB2CWeb.Models;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
 
 namespace AzureADB2CWeb.Services
 {
     public class UserService : IUserService
     {
         private readonly ApplicationDbContext _dbContext;
-        public UserService(ApplicationDbContext dbContext)
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public UserService(ApplicationDbContext dbContext, IHttpContextAccessor httpContextAccessor)
         {
             _dbContext = dbContext;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public User Create(User user)
         {
-            throw new NotImplementedException();
+            _dbContext.Users.Add(user);
+            _dbContext.SaveChanges();
+            return user;
         }
 
-        public Task<string> GetB2CTokenAsync()
+        public async Task<string?> GetB2CTokenAsync()
         {
-            throw new NotImplementedException();
+            try
+            {
+                return await _httpContextAccessor!.HttpContext!.GetTokenAsync("access_token");
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
 
-        public User GetById(string b2cObjectId)
+        public User? GetById(string b2cObjectId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return _dbContext!.Users!.FirstOrDefault(u => u.B2CObjectId == b2cObjectId);
+            }
+            catch (Exception ex)
+            {
+                return new User();
+            }
         }
 
         public User GetUserFromSession()
         {
-            throw new NotImplementedException();
+            // Here we want to put a "User" object into the Session,so that we can know all infromation about this User
+            // The B2C cannot provide all fields( customer field need graph api to retrieve)
+            var user = _httpContextAccessor.HttpContext.Session.GetComplexData<User>("UserSession");
+            if (user == null || string.IsNullOrWhiteSpace(user.B2CObjectId))
+            {
+                var idClaim = ((ClaimsIdentity)_httpContextAccessor.HttpContext.User.Identity).FindFirst(ClaimTypes.NameIdentifier);
+                string userId = idClaim.Value;
+                user = GetById(userId);
+                _httpContextAccessor.HttpContext.Session.SetComplexData("UserSession", user);
+            }
+            return user;
         }
     }
 }
