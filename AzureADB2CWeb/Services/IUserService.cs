0784ed8d7f8d358f0e60148657cf7517d15f0c28
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-04-12 14:45:02 -0400

    5-5 User service interface

---
+++ AzureADB2CWeb/Services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using AzureADB2CWeb.Models;
+
+namespace AzureADB2CWeb.Services
+{
+    public interface IUserService
+    {
+        User Create(User user); // How come we have such a wired method?
+        User GetById(string b2cObjectId);
+        Task<string> GetB2CTokenAsync();
+        User GetUserFromSession();
+    }
+}
---
+++ AzureADB2CWeb/Services/UserService.cs
@@ -0,0 +1,34 @@
+﻿using AzureADB2CWeb.Data;
+using AzureADB2CWeb.Models;
+
+namespace AzureADB2CWeb.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public UserService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public User Create(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetB2CTokenAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetById(string b2cObjectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUserFromSession()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
