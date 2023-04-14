43d75a8cc063a9f2fc9296effe17973d618d5385
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-04-14 09:51:25 -0400

    6-6 Can access API now

--- AzureADB2CApi/Program.cs
+++ AzureADB2CApi/Program.cs
@@ -5,11 +5,28 @@ namespace AzureADB2CApi
 {
     public class Program
     {
+
+
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+            /*
+             options.Authority = $"https://azureadb2cmikedomain.b2clogin.com/AzureADB2CmikeDomain.onmicrosoft.com/{policy}/v2.0/";
+            options.ClientId = "570c99e9-8b2e-45c9-97d9-ebebf5f141de";
+            options.ResponseType = "code";    // when authenticate with "secret" other than "token"
+            options.SaveTokens = true;
+            options.Scope.Add(options.ClientId); // same as ClientId
+            options.ClientSecret = "Sl18Q~PhmN6gu~ChyFpySKLYQetcvkjQCt-4Jc~P";
+            options.CallbackPath = "/signin-oidc-" + policy;
+            options.TokenValidationParameters = new TokenValidationParameters { NameClaimType = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname" };
+             */
+
+
+
+
             // Here add services and setup it.
             builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddMicrosoftIdentityWebApi(
